bccd7df8b7c68811b84f22e715b2c301dfea70a8
Andreas Kissavos <kissavos@gmail.com>
2013-12-10 22:46:33 +0100

    solved c small

--- 2008Practise/PracticeProblems/EggDrop.py
+++ 2008Practise/PracticeProblems/EggDrop.py
@@ -1,42 +1,33 @@
 
-def fibo(n):
-    sum = 0
-    while True:
 
-def calcFPrime(D,B):
+
+def calcFPrime(D,B,F):
     FPrime = 0
-    while True:
-      if (B == 0 or D == 0):
-          return FPrime
-      elif(B == D):
-          FPrime = FPrime + 2**(B)-1
-      else:
-        FPrime = FPrime + 1
-
-    return FPrime
-
-def trisum(n, csum):
-...     while True:                     # change recursion to a while loop
-...         if n == 0:
-...             return csum
-...         n, csum = n - 1, csum + n
+#    print D
+#    print B
+#    print F
+    if ((B == 0) or (D == 0)):
+        return 0
+    elif(D == 1):
+        return 1
+    else:
+        return 1 + F[str([D-1,B])] + F[str([D-1,B-1])]
 
 
-
-def calcDPrime(F,B):
+def calcDPrime(F,B,Fos):
     DPrime = 0
     for i in range(0,F+1):
-        FPrime = calcFPrime(i,B)
+        FPrime = calcFPrime(i,B,Fos)
         if(FPrime >= F):
             DPrime = i
             break
 
     return DPrime
 
-def calcBPrime(F,D):
+def calcBPrime(F,D,Fos):
     BPrime = 0
     for i in range(D,-1,-1):
-        FPrime = calcFPrime(D,i)
+        FPrime = calcFPrime(D,i,Fos)
 #        print FPrime
         if(FPrime < F):
             BPrime = i+1
@@ -49,28 +40,43 @@ def eggDrop ():
     l = 0
     x = [0,0]
     y = [0,0]
-    f = open ("c:\\gcj\\2008Practise\\PracticeProblems\\C-small-practice.in",'r')
-    g = open ("c:\\gcj\\2008Practise\\PracticeProblems\\C-small-practice.out",'w')
+    f = open ("/home/andreask/gcj/2008Practise/PracticeProblems/C-large-practice.in",'r')
+    g = open ("/home/andreask/gcj/2008Practise/PracticeProblems/C-large-practice.out",'w')
     lines = f.readlines()
     nCases = int(lines[0])
     lines.pop(0)
     case = 0
-    lines = [lines[3]]
+    Fos = {}
+
+
+    for j in range (0,1000):
+      for i in range(0,1000):
+          cFP = calcFPrime(i, j, Fos)
+          Fos[str([i,j])] = cFP
+
+
     for line in lines:
-        case = 0
+        case = case + 1
         bla = line.split()
         F = int(bla[0])
         D = int(bla[1])
         B = int(bla[2])
-        FPrime = calcFPrime(D,B)
-        DPrime = calcDPrime(F,B)
-        BPrime = calcBPrime(F,D)
-        g.write("Case #" + str(case) + ": " + str(FPrime) + " " + str(DPrime) + " " + str(BPrime))
+        if(B > 32):
+            FPrime = -1
+        else:
+            FPrime = calcFPrime(D,B,Fos)
+
+        if(FPrime > 2**32):
+            FPrime = -1
+        DPrime = calcDPrime(F,B,Fos)
+        BPrime = calcBPrime(F,D,Fos)
+        g.write("Case #" + str(case) + ": " + str(FPrime) + " " + str(DPrime) + " " + str(BPrime) + '\n')
+
 
 
 # F D B (floors, drops, broken)
 # algorithm for F': if D = B: F' = 2**B-1
-# otherwise: F' = D*(D-1)/2(geometric sum)+ 1 + F'((D-1),B)
+# otherwise: F(D,B) = F(D-1,B-1) + 1 + F((D-1),B)
 
 # for D': go from D = B and calulate F' until it is lager or equal to F
 # for B': go from B = D and go down until F' is below F. Then take the one below.
@@ -80,10 +86,19 @@ def eggDrop ():
 
 eggDrop()
 
-calcFPrime(20,6)
 
+2**32
+def test ():
+    t = [[i*j for i in range(0,5)] for j in range(0,4)]
 
+    return t[3][3]
 
+test()
+
+for i in range(0,3):
+    for j in range(0,2):
+
+        print [i,j]
 
 
 
