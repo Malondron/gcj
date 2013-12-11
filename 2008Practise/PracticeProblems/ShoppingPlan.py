52c495b0559982502730cb3934f250a9b2783789
Andreas Kissavos <kissavos@gmail.com>
2013-12-11 23:08:10 +0100

    started d, reading in done, now more done

--- 2008Practise/PracticeProblems/ShoppingPlan.py
+++ 2008Practise/PracticeProblems/ShoppingPlan.py
@@ -1,5 +1,8 @@
 
 
+
+#end with perishable. Start from the last. If at store where have bought item, and there is another item,
+#it is always preferrable to buy the other as well, compared to another store, if the price plus gas to another store is lower.
 def shoppingPlan():
     nCases = 0
 #    f = open ("c:\\gcj\\2008Practise\\PracticeProblems\\C-large-practice.in",'r')
@@ -9,11 +12,14 @@ def shoppingPlan():
     lines = f.readlines()
     nCases = int(lines[0])
     l=0
-    for i in range(1,2):
+    for i in range(2,3):
         l = l+1
         nItems, nStores, gasPrice = map(int,lines[l].split())
         l = l+1
         items = [x for x in lines[l].split()]
+        perishItems = [x[0:-1] for x in items if (x[-1] == '!')]
+        for per in perishItems:
+            items.remove(per + '!')
         stores = []
         for j in range(0,nStores):
             store = {}
@@ -31,11 +37,15 @@ def shoppingPlan():
             stores.append(store)
 
 
-    return stores
+    return items
 
 "dd:4488".split(':')
 map(int,["23", "56"])
 shoppingPlan()
 
 
+[1,2,4,5].remove(1)
+"hej"[-1]
+import math
+200+math.sqrt(18)*5+math.sqrt(7**2+9)*5+ 90 + 150+20
 
