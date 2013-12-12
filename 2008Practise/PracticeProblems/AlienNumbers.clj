8e6dba50cdc66d6b13efe1e60bfa0555fd8cdd19
Andreas Kissavos <kissavos@gmail.com>
2013-12-12 18:04:40 +0100

    added clj a more

--- 2008Practise/PracticeProblems/AlienNumbers.clj
+++ 2008Practise/PracticeProblems/AlienNumbers.clj
@@ -20,23 +20,26 @@
     (loop [sum 0 i 0]
       (if (= i num-length)
         sum
-        (recur (+ sum (* (Math/pow base (- num-length (+ base 1))) (.indexOf source-lang (nth number i)))) (inc i)) ))))
-
+        (do
+          (print sum)
+        (recur (+ sum (* (Math/pow base (- num-length (+ base 1))) (.indexOf source-lang (str (nth number i))))) (inc i)) )))))
 
 (defn translate-number [number source-lang target-lang]
   (let [num-len (count number)
         base (count source-lang)
         new-base (count target-lang)
-        value-to-translate (to-base-ten number source-lang)
+        value-to-translate (to-base-ten number source-lang base)
         largest-exponent (Math/floor (/ (Math/log value-to-translate) (Math/log new-base)))]
-    largest-exponent))
+    value-to-translate))
 
 (defn alien-numbers []
   (let [lines (read-lines "c:\\gcj\\2008Practise\\PracticeProblems\\A-small-practice.in")
         nCases (read-string (first lines))]
-   (doseq [l (rest lines)]
+   (doseq [l [(second lines)]]
      (let[l-parse (split l #" ")
-          out (translate-number l-parse)]
-       (write-lines "c:\\gcj\\2008Practise\\PracticeProblems\\A-small-practice.out.clj" out)))))
+          out (translate-number (first l-parse) (second l-parse) (nth l-parse 2))]
+       (print out)))))
+;       (write-lines "c:\\gcj\\2008Practise\\PracticeProblems\\A-small-practice.out.clj" out)))))
 
 (alien-numbers)
+(.indexOf "hej" (str(second "he")))
